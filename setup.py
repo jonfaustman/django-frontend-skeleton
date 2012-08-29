e2e7c84c68b3edadabeac332d7a6efb463f7ec62
Jon Faustman <faustman@k-state.edu>
2012-08-29 13:32:12 -0500

    Initial setup.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(name='django-front-end-skeleton',
+      version=version,
+      description="A Collection of Bootstrap static files.",
+      long_description=open("README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Framework :: Django",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='',
+      author='Jon Faustman',
+      author_email='jon.faustman@gmail.com',
+      url='http://github.com/jonfaustman/django-front-end-skeleton',
+      license='MIT',
+      packages=find_packages(),
+      install_requires = [],
+      include_package_data=True,
+      zip_safe=False,
+    )
